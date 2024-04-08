492f7c0a7dab25a663a768ce7a8640ea3c08232b
Pavel Kotlov <pavelkotlov@gmail.com>
2024-04-08 15:59:02 -0700

    Add template rendering for login and logout views

--- store/views.py
+++ store/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .models import Product
+
 
 # Create your views here.
 def home(request):
@@ -9,3 +12,26 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html', {})
+
+def login_user(request):
+  # If login form was submitted process login request, else render login.html template.
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+      login(request, user)
+      messages.success(request, ('You have been logged in succesfully'))
+      return redirect('home')
+    else:
+      messages.success(request, ('Login unsuccessful'))
+      return redirect('login')
+    
+  else:
+    return render(request, 'login.html', {})
+
+def logout_user(request):
+  logout(request)
+  messages.success(request, ('You have been logged out successfully.'))
+  return redirect('home')
