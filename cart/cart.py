77d722bc9943edb6a89e319e2aab4d76b201b980
Pavel Kotlov <pavelkotlov@gmail.com>
2024-04-09 23:17:59 -0700

    Add class of Cart and init with a session key

--- cart/cart.py
+++ cart/cart.py
@@ -5,6 +5,9 @@ class Cart():
     # Retrieve current session key if exists
     cart = self.session.get('session_key')
 
+    # If no session key present create one
     if 'session_key' not in request.session:
       cart = self.session['session_key'] = {}
-    pass
+    
+    # Make cart avaialble on all pages
+    self.cart = cart
