ea14be9769b619db0b6d3b3130498ecc1c304836
Pavel Kotlov <pavelkotlov@gmail.com>
2024-04-11 15:29:26 -0700

    Add category summary view to point to template and pass categories

--- store/views.py
+++ store/views.py
@@ -7,6 +7,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from .forms import SignUpForm
 
+def category_summary(request):
+  categories = Category.objects.all()
+
+  return render(request, "category_summary.html", {"categories":categories})
+
 def category(request, cat):
   # replace category name hyphens with spaces 
   cat = cat.replace('-', ' ')
