69d48f8e6cad632018824a0b02b8ebe839c3da2d
Pavel Kotlov <pavelkotlov@gmail.com>
2024-04-11 15:46:09 -0700

    Add messages to product to cart add, delete, and update

--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
+from django.contrib import messages
 
 def cart_summary(request):
   cart = Cart(request)
@@ -25,7 +26,7 @@ def cart_add(request):
     cart_quantity = cart.__len__()
     
     response = JsonResponse({'qty': cart_quantity})
-    
+    messages.success(request, ('Product added to cart'))
     return response
 
 def cart_delete(request):
@@ -37,7 +38,7 @@ def cart_delete(request):
     cart.delete(product=product_id)
 
     response = JsonResponse({'product':product_id})
-    
+    messages.success(request, ('Product removed from cart'))
     return response
 
 def cart_update(request):
@@ -50,5 +51,5 @@ def cart_update(request):
     cart.update(product=product_id, quantity=product_qty)
 
     response = JsonResponse({'qty':product_qty})
-    
+    messages.success(request, ('Product updated'))
     return response
