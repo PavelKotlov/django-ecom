62d7f950fb7943ddd62f0bb03f6d71a861e1bdc3
Pavel Kotlov <pavelkotlov@gmail.com>
2024-04-15 03:38:54 -0700

    Refactor update info view to render two forms billing and shipping

--- store/views.py
+++ store/views.py
@@ -6,21 +6,27 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
+from payment.forms import ShippingForm
+from payment.models import ShippingAddress
 import json
 from cart.cart import Cart
 
 def update_info(request):
   if request.user.is_authenticated:
     current_user = Profile.objects.get(user__id=request.user.id)
-    form = UserInfoForm(request.POST or None, instance=current_user)
+    shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
 
-    if form.is_valid():
+    form = UserInfoForm(request.POST or None, instance=current_user)
+    shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+
+    if form.is_valid() or shipping_form.is_valid():
       form.save()
+      shipping_form.save()
       
       messages.success(request, ('Profile info saved successfully'))
       return redirect('home')
     
-    return render(request, 'update_info.html', {'form':form})
+    return render(request, 'update_info.html', {'form':form, 'shipping_form':shipping_form})
   else:
     messages.success(request, ('Access denied, please login first'))
     return redirect('login')
