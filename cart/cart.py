bcc4df29cf4134f6812afd2c0ec5c48b967bf62b
Pavel Kotlov <pavelkotlov@gmail.com>
2024-04-15 02:13:01 -0700

    Add db_add class method for cart persistance refactor existing update, and delete methods to support same

--- cart/cart.py
+++ cart/cart.py
@@ -33,7 +33,26 @@ class Cart():
       cart = cart.replace("\'", "\"")
       # Update old cart field in user profile with a str of cart items in dict format
       current_user.update(old_cart=cart)
-    
+  
+  def db_add(self, product, cart_quantity):
+    product_id = str(product)
+    product_qty = str(cart_quantity)
+
+    if product_id not in self.cart:
+      self.cart[product_id] = int(product_qty)
+
+    self.session.modified = True
+
+    # Cart persistance
+    if self.request.user.is_authenticated:
+      # If user is authenticated grab the user profile
+      current_user = Profile.objects.filter(user__id=self.request.user.id)
+      # Grab the cart items convert them to str and prep for JSON
+      cart = str(self.cart)
+      cart = cart.replace("\'", "\"")
+      # Update old cart field in user profile with a str of cart items in dict format
+      current_user.update(old_cart=cart)
+
   def cart_total(self):
     product_ids = self.cart.keys()
     products = Product.objects.filter(id__in=product_ids)
@@ -75,6 +94,16 @@ class Cart():
 
     self.session.modified = True
 
+    # Cart persistance
+    if self.request.user.is_authenticated:
+      # If user is authenticated grab the user profile
+      current_user = Profile.objects.filter(user__id=self.request.user.id)
+      # Grab the cart items convert them to str and prep for JSON
+      cart = str(self.cart)
+      cart = cart.replace("\'", "\"")
+      # Update old cart field in user profile with a str of cart items in dict format
+      current_user.update(old_cart=cart)
+      
     ourcart = self.cart
     return ourcart
   
@@ -85,4 +114,14 @@ class Cart():
       del self.cart[product_id]
     
     self.session.modified = True
+
+    # Cart persistance
+    if self.request.user.is_authenticated:
+      # If user is authenticated grab the user profile
+      current_user = Profile.objects.filter(user__id=self.request.user.id)
+      # Grab the cart items convert them to str and prep for JSON
+      cart = str(self.cart)
+      cart = cart.replace("\'", "\"")
+      # Update old cart field in user profile with a str of cart items in dict format
+      current_user.update(old_cart=cart)
   
