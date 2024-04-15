e4dfcd5bc6698c7c69b51b32722d8254b84e956d
Pavel Kotlov <pavelkotlov@gmail.com>
2024-04-15 13:32:12 -0700

    Add process order view logic and refactor billing info to add shipping session

--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from cart.cart import Cart
 from .forms import ShippingForm, PaymentForm
-from .models import ShippingAddress
+from .models import ShippingAddress, Order, OrderItem
 from django.contrib import messages
 
 
@@ -29,6 +29,9 @@ def billing_info(request):
     quantities = cart.get_quantities()
     totals = cart.cart_total()
 
+    my_shipping = request.POST 
+    request.session['my_shipping'] = my_shipping
+
     if request.user.is_authenticated:
       billing_form = PaymentForm()
 
@@ -42,4 +45,53 @@ def billing_info(request):
   
   else:
     messages.success(request, 'Access Denied.')
+    return redirect('home')
+  
+def process_order(request):
+  if request.POST:
+    # Get the cart
+    cart = Cart(request)
+    cart_products = cart.get_products()
+    quantities = cart.get_quantities()
+    totals = cart.cart_total()
+
+    # Get Billing Info from the last page
+    payment_form = PaymentForm(request.POST or None)
+    # Get Shipping Session Data
+    my_shipping = request.session.get('my_shipping')
+
+    # Gather Order Info
+    full_name = my_shipping['shipping_full_name']
+    email = my_shipping['shipping_email']
+    # Create Shipping Address from session info
+    shipping_address = f"{my_shipping['shipping_address1']}\n{my_shipping['shipping_address2']}\n{my_shipping['shipping_city']}\n{my_shipping['shipping_state']}\n{my_shipping['shipping_zipcode']}\n{my_shipping['shipping_country']}"
+    amount_paid = totals
+
+    # Create an Order
+    if request.user.is_authenticated:
+      # logged in
+      user = request.user
+      # Create Order
+      create_order = Order(user=user, full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
+      create_order.save()
+      for product in cart_products:
+              print(product)
+              cart.delete(product.id)
+      messages.success(request, "Order Placed!")
+      return redirect('home')
+
+    else:
+      # not logged in
+      # Create Order
+      create_order = Order(full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
+      create_order.save()
+      for product in cart_products:
+        print(product)
+        cart.delete(product.id)
+
+      messages.success(request, "Order Placed!")
+      return redirect('home')
+
+  else:
+    messages.success(request, "Access Denied")
     return redirect('home')
