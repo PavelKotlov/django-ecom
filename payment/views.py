58d8191c8615d5600105e4aeb7f4f0960424f32e
Pavel Kotlov <pavelkotlov@gmail.com>
2024-04-15 13:12:51 -0700

    Add billing info view logic to process payment details

--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cart.cart import Cart
-from .forms import ShippingForm
+from .forms import ShippingForm, PaymentForm
 from .models import ShippingAddress
+from django.contrib import messages
+
 
 def payment_success(request):
   return render(request, 'payment/payment_success.html', {})
@@ -19,3 +21,25 @@ def checkout(request):
   else:
     shipping_form = ShippingForm(request.POST or None)
     return render(request, "payment/checkout.html", {'cart_products':cart_products, 'product_quantities': quantities, 'totals': totals, 'shipping_form':shipping_form})
+  
+def billing_info(request):
+  if request.POST:
+    cart = Cart(request)
+    cart_products = cart.get_products()
+    quantities = cart.get_quantities()
+    totals = cart.cart_total()
+
+    if request.user.is_authenticated:
+      billing_form = PaymentForm()
+
+      return render(request, "payment/billing_info.html", {'cart_products':cart_products, 'product_quantities': quantities, 'totals': totals, 'shipping_info':request.POST, 'billing_form':billing_form})
+    else:
+      billing_form = PaymentForm()
+      return render(request, "payment/billing_info.html", {'cart_products':cart_products, 'product_quantities': quantities, 'totals': totals, 'shipping_form':request.POST, 'billing_form':billing_form})
+    
+    shipping_form = request.POST
+    return render(request, "payment/billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form})	
+  
+  else:
+    messages.success(request, 'Access Denied.')
+    return redirect('home')
